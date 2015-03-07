57ea79ecc3c43044775a4d2a2c1a98db48a563dc
James Bell <james.bell@gmail.com>
2015-03-07 23:27:48 +0000

    Removes deprecated 2.3 view and secret usage

--- config/initializers/cookie_verification_secret.rb
+++ config/initializers/cookie_verification_secret.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '1a8ff308daa1e89e43c8008c709584d64206a1c82a5496bd74fbdde382c6d3dc41634ee33552070437ed7c064816a285c3e6b86d35d82c4ea9d714f0aed420fd';
+ActionController::Base.config.secret_token = '1a8ff308daa1e89e43c8008c709584d64206a1c82a5496bd74fbdde382c6d3dc41634ee33552070437ed7c064816a285c3e6b86d35d82c4ea9d714f0aed420fd';

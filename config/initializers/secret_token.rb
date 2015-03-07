207503ee5df13e2d42ca7b85228f0a6edc8b551f
James Bell <james.bell@gmail.com>
2015-03-07 22:25:30 +0000

    Upgrades to ruby 2.1 and Rails 3.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thanksamillion::Application.config.secret_token = '76d61c1a7c18ce678628a15b24300a12b49be94aa8087c3bcfe8e0d86c79a95f4afe9de1d6dd07c4a53a13ced91e89a1424b6f909498f2934ba5dffc525efd3d'

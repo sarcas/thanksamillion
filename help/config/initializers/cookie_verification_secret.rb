8e738bf02628171076bd84d5b3b060eccae18b1b
James Bell <james.bell@gmail.com>
2015-03-08 09:11:41 +0000

    Removing old erroneous rails app 'help'
    I must have created this by trying to get help from the rails command
    line tool.

--- help/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '18a0d819c66a3e8dd3930d3c0ebda80e76c9a3f9b503fbf1fe922ae799e374080dbf5464fddef8a6444dda809f4c68b30185493a6202ff66d2f9fd046c4f2494';
